a7c29bde23523135339afb371c276410f3c52d27
JuanDiegoCC02 <dcorella2324@gmail.com>
2026-03-15 17:45:12 -0600

    Update Structure AddCamera / Cameras

--- FrontEnd-Networks-Telecommunications/src/components/AddCamera.jsx
+++ FrontEnd-Networks-Telecommunications/src/components/AddCamera.jsx
@@ -1,8 +1,51 @@
 import React from 'react'
 
 function AddCamera() {
+
+
   return (
-    <div>AddCamera</div>
+    <div>
+
+      <div><h3>Add New Camera Connection</h3></div>
+
+      <div>
+
+        <div>
+         <h4>Camera Connection</h4>
+        </div>
+
+        <div>
+          <label htmlFor=""> Camera Name </label>
+          <input type="text" />
+        </div>        
+        <div>
+          <label htmlFor=""> IP Access Camera </label>
+          <input type="text" />
+        </div>     
+        <div>
+          <label htmlFor=""> Camera Location </label>
+          <input type="text" />
+        </div>    
+           <div>
+          <label htmlFor=""> Camera Description </label>
+          <input type="text" />
+        </div>        
+        <div>
+          <label htmlFor=""> Camera Status </label>
+          <select name="" id="">
+            <option value="">select status</option>
+            <option value="on">ON</option>
+            <option value="off">OFF</option>
+          </select>
+        </div>
+
+        <div>
+          <button>Add Camera</button>
+        </div>
+
+      </div>
+
+    </div>
   )
 }
 

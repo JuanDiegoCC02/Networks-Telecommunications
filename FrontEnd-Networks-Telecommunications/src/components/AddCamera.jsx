b0c2ad7a429d16c5381ca6b56ee68061ca309652
JuanDiegoCC02 <dcorella2324@gmail.com>
2026-03-16 21:32:32 -0600

    Update form structure logic Add camera / Add Camera

--- FrontEnd-Networks-Telecommunications/src/components/AddCamera.jsx
+++ FrontEnd-Networks-Telecommunications/src/components/AddCamera.jsx
@@ -1,6 +1,71 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { postCameras } from '../services/camerasApi';
 
 function AddCamera() {
+  const [cameraName, setCameraName] = useState("");
+  const [cameraIP, setCameraIP] = useState("");
+  const [cameraLocation, setCameraLocation] = useState("");
+  const [cameraDescription, setCameraDescroption] = useState("");
+  const [cameraStatus, setCameraStatus] = useState("");
+
+  function name(e) {
+    setCameraName(e.target.value)    
+  }
+   function ip(e) {
+    setCameraIP(e.target.value)    
+  }
+   function location(e) {
+    setCameraLocation(e.target.value)    
+  }
+   function description(e) {
+    setCameraDescroption(e.target.value)    
+  }
+   function status(e) {
+    setCameraStatus(e.target.value)    
+  }
+
+
+  //create connection camera
+    async function create() {
+
+      console.log("name:", cameraName)
+      console.log("ip:", cameraIP)
+      console.log("location:", cameraLocation)
+      console.log("description:", cameraDescription)
+      console.log("status:", cameraStatus)
+      
+  if (!cameraName || !cameraIP || !cameraLocation || !cameraDescription || !cameraStatus) {
+      alert("Complete all fields");
+      return;
+  }
+
+  const obj = {
+      name: cameraName,
+      ip_camera: cameraIP,
+      location: cameraLocation,
+      description: cameraDescription,
+      status: cameraStatus
+  }
+
+  try {
+
+      const requestServer = await postCameras(obj);
+
+      console.log("camera register:", requestServer);
+
+      alert("camera connection created successfully");
+
+     
+
+  } catch (error) {
+
+      console.error("Error for create camera:", error);
+
+      alert("Error creating camera");
+
+  }
+}
+
 
 
   return (
@@ -16,31 +81,31 @@ function AddCamera() {
 
         <div>
           <label htmlFor=""> Camera Name </label>
-          <input type="text" />
+          <input type="text" placeholder='Insert Name' value={cameraName} onChange={name} />
         </div>        
         <div>
           <label htmlFor=""> IP Access Camera </label>
-          <input type="text" />
+          <input type="text"  placeholder='Insert IP' value={cameraIP} onChange={ip}  />
         </div>     
         <div>
           <label htmlFor=""> Camera Location </label>
-          <input type="text" />
+          <input type="text"  placeholder='Insert Location' value={cameraLocation} onChange={location}  />
         </div>    
            <div>
           <label htmlFor=""> Camera Description </label>
-          <input type="text" />
+          <input type="text"  placeholder='Insert Description' value={cameraDescription} onChange={description}  />
         </div>        
         <div>
           <label htmlFor=""> Camera Status </label>
-          <select name="" id="">
+          <select value={cameraStatus} onChange={status} name="" id="">
             <option value="">select status</option>
-            <option value="on">Active</option>
-            <option value="off">Inactive</option>
+            <option value="active">Active</option>
+            <option value="inactive">Inactive</option>
           </select>
         </div>
 
         <div>
-          <button>Add Camera</button>
+          <button onClick={create} value="Create">Add Camera</button>
         </div>
 
       </div>

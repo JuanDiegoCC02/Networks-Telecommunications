c41d3312dce4c033957248d6481f0234251f7607
Juan Diego C.C. <dcorella2324@gmail.com>
2026-05-02 23:18:52 -0600

    Update Add New Structure to ContactUsBody / ContactUs

--- FrontEnd-Networks-Telecommunications/src/components/ContactUsBody.jsx
+++ FrontEnd-Networks-Telecommunications/src/components/ContactUsBody.jsx
@@ -2,7 +2,30 @@ import React from 'react'
 
 function ContactUsBody() {
   return (
-    <div>ContactUsBody</div>
+    <div>
+
+    <div><h1> Contact Us</h1></div>
+
+    <div>
+    <span>Information WEB Site</span>
+    </div>
+
+    <div>
+      <span>Story</span>
+    </div>
+
+    <div>
+      <span>Contact Information</span>
+      <span>Contact Information</span>
+      <span>Contact Information</span>
+
+    </div>
+
+     <div>
+      <span>Contact Information</span>
+    </div>
+
+    </div>
   )
 }
 

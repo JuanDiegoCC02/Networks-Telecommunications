0366ab9ec6a5e90cea0a6b7868892ffc0c0a7016
Juan Diego C.C. <dcorella2324@gmail.com>
2026-05-05 23:56:13 -0600

    Update Add New Structure HomeBody / Home

--- FrontEnd-Networks-Telecommunications/src/components/HomeBody.jsx
+++ FrontEnd-Networks-Telecommunications/src/components/HomeBody.jsx
@@ -2,7 +2,46 @@ import React from 'react'
 
 function HomeBody() {
   return (
-    <div>HomeBody</div>
+    <div>
+
+      <div><h1>title home</h1></div>
+
+      <div>
+        <h3>subtitle home</h3>
+        <p>description home</p>
+      </div>
+
+      <div>
+        <div>
+          <span>Objetives</span>
+          <div>
+            <ul>
+              <li>objetive 1</li>
+            </ul>
+          </div>
+        </div>
+        <div>
+          <span>Goals</span>
+          <div>
+            <ul>
+              <li>goal 1</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <div>
+          <p>description img website</p>
+        </div>
+
+        <div>
+          <p>description website</p>
+        </div>
+        
+      </div>
+
+    </div>
   )
 }
 

31d61815dfba6eb38c3c814998e7000d5ed368e6
Juan Diego C.C. <dcorella2324@gmail.com>
2026-05-22 21:57:18 -0600

    Update Add Adjustments to the structure addRouters create input thumbnail_url / Routers

--- FrontEnd-Networks-Telecommunications/src/components/AddRouters.jsx
+++ FrontEnd-Networks-Telecommunications/src/components/AddRouters.jsx
@@ -10,11 +10,40 @@ function AddRouters() {
   const [mac_Address, setMac_Address] = useState("");
   const [routerBrand, setRouterBrand] = useState("");
   const [routerModel, setRouterModel] = useState("");
+  const [Url_thumbnail, setUrlThumbnail] = useState("");
   const [routerLocation, setRouterLocation] = useState("");
   const [latitude, setLatitude] = useState(null);
   const [longitude, setLongitude] = useState(null);
   const [routerStatus, setRouterStatus] = useState("");
   const handleCoords = (coords) => { setLatitude(coords.lat);  setLongitude(coords.lng); };
+
+    function name(e) {
+    setRouterName(e.target.value)    
+  }
+    function description(e) {
+    setRouterDescription(e.target.value)
+  }  
+   function ip_address(e) {
+    setIp_Address(e.target.value)    
+  }
+    function mac_address(e) {
+    setMac_Address(e.target.value)
+  }
+    function brand(e) {
+    setRouterBrand(e.target.value)
+  }
+    function model(e) {
+    setRouterModel(e.target.value)
+  }
+    function thumbnail_url(e) {
+    setUrlThumbnail(e.target.value)
+  }
+    function location(e) {
+    setRouterLocation(e.target.value)
+  }
+    function status(e) {
+    setRouterStatus(e.target.value)
+  }
 
   // reset form
   const resetForm = () => {
@@ -24,6 +53,7 @@ function AddRouters() {
     setMac_Address("");
     setRouterBrand("");
     setRouterModel("");
+    setUrlThumbnail("");
     setRouterLocation("");
     setRouterStatus("");
     setLatitude(null);
@@ -38,13 +68,14 @@ function AddRouters() {
     console.log("mac_address:", mac_Address);
     console.log("brand:", routerBrand);
     console.log("model:", routerModel);
+    console.log("thumbnail_url:", Url_thumbnail);
     console.log("location:", routerLocation);
     console.log("latitude:", latitude);
     console.log("longitude:", longitude);
     console.log("status:", routerStatus);
     
     // validate inputs
-    if (!routerName || !ip_Address || !mac_Address || !routerBrand || !routerModel || !routerLocation || !routerStatus) {
+    if (!routerName || !ip_Address || !mac_Address || !routerBrand || !routerModel || !Url_thumbnail || !routerLocation || !routerStatus) {
       alert("Complete all fields");
       return;
     }
@@ -56,6 +87,7 @@ function AddRouters() {
       mac_address: mac_Address,
       brand: routerBrand,
       model: routerModel,
+      thumbnail_url: Url_thumbnail,
       location: routerLocation,
       latitude: latitude,
       longitude: longitude,
@@ -86,77 +118,45 @@ function AddRouters() {
 
         <div className='addRoutersContainerForm'>
           <label className='addRoutersLabelForm'> Name </label>
-          <input 
-            className='addRoutersInputForm' 
-            placeholder='Insert Name' 
-            type="text" 
-            value={routerName} 
-            onChange={(e) => setRouterName(e.target.value)}
+          <input className='addRoutersInputForm' placeholder='Insert Name' type="text" value={routerName} onChange={(e) => name(e)}
           />
         </div>
 
         <div className='addRoutersContainerForm'>
           <label className='addRoutersLabelForm'> Description </label>
-          <input 
-            className='addRoutersInputForm' 
-            placeholder='Insert Description' 
-            type="text" 
-            value={routerDescription} 
-            onChange={(e) => setRouterDescription(e.target.value)}
-          />
+          <input className='addRoutersInputForm' placeholder='Insert Description' type="text" value={routerDescription} onChange={(e) => description(e)}/>
         </div>
 
         <div className='addRoutersContainerForm'>
           <label className='addRoutersLabelForm'> IP Address </label>
-          <input 
-            className='addRoutersInputForm' 
-            placeholder='Insert IP Address' 
-            type="text" 
-            value={ip_Address} 
-            onChange={(e) => setIp_Address(e.target.value)}
-          />
+          <input className='addRoutersInputForm' placeholder='Insert IP Address' type="text" value={ip_Address} onChange={(e) => ip_address(e)}/>
         </div>
 
         <div className='addRoutersContainerForm'>
           <label className='addRoutersLabelForm'> MAC Address </label>
-          <input 
-            className='addRoutersInputForm' 
-            placeholder='Insert MAC Address' 
-            type="text" 
-            value={mac_Address} 
-            onChange={(e) => setMac_Address(e.target.value)}
-          />
+          <input className='addRoutersInputForm' placeholder='Insert MAC Address' type="text" value={mac_Address} onChange={(e) => mac_address(e)}/>
         </div>
 
         <div className='addRoutersContainerForm'>
           <label className='addRoutersLabelForm'> Brand </label>
-          <input 
-            className='addRoutersInputForm' 
-            placeholder='Insert Brand' 
-            type="text" 
-            value={routerBrand} 
-            onChange={(e) => setRouterBrand(e.target.value)}
-          />
+          <input className='addRoutersInputForm' placeholder='Insert Brand' type="text" value={routerBrand} onChange={(e) => brand(e)}/>
         </div>
 
         <div className='addRoutersContainerForm'>
           <label className='addRoutersLabelForm'> Model </label>
-          <input 
-            className='addRoutersInputForm' 
-            placeholder='Insert Model' 
-            type="text" 
-            value={routerModel} 
-            onChange={(e) => setRouterModel(e.target.value)}
-          />
+          <input className='addRoutersInputForm' placeholder='Insert Model' type="text" value={routerModel} onChange={(e) => model(e)}/>
         </div>
 
         <div className='addRoutersContainerForm'>
+          <label className='addRoutersLabelForm'> Thumbnail URL </label>
+          <input className='addRoutersInputForm' placeholder='Insert Thumbnail URL' type="text" value={routerModel} onChange={(e) => thumbnail_url(e)}/>
+        </div>
+        
+
+
+        <div className='addRoutersContainerForm'>
           <label className='addRoutersLabelForm'>Location</label>
-          <select 
-            className='addRoutersSelectLocation' 
-            value={routerLocation} 
-            onChange={(e) => setRouterLocation(e.target.value)}
-          >
+          <select className='addRoutersSelectLocation' value={routerLocation} onChange={(e) => location(e)}>
             <option className='addRoutersOptionSelect' value="">Select Location</option>
             <option className='addRoutersOptionSelect' value="San José">San José</option>
             <option className='addRoutersOptionSelect' value="Cartago">Cartago</option>
@@ -170,11 +170,7 @@ function AddRouters() {
 
         <div className='addRoutersContainerForm'>
           <label className='addRoutersLabelForm'>Status</label>
-          <select 
-            className='addRoutersSelectStatus' 
-            value={routerStatus} 
-            onChange={(e) => setRouterStatus(e.target.value)}
-          >
+          <select className='addRoutersSelectStatus' value={routerStatus} onChange={(e) => status(e)}>
             <option className='addRoutersOptionSelect' value="">Select Status</option>
             <option className='addRoutersOptionSelect' value="Active">Active</option>
             <option className='addRoutersOptionSelect' value="Maintenance">Maintenance</option>

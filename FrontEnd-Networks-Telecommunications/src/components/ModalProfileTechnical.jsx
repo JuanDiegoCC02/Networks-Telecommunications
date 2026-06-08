db855ae898d9a60385dd0b756621704f2d692a97
Juan Diego C.C. <dcorella2324@gmail.com>
2026-06-07 22:17:52 -0600

    feat: create new modal proofile technical / ModalProfile

---
+++ FrontEnd-Networks-Telecommunications/src/components/ModalProfileTechnical.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ModalProfileTechnical() {
+  return (
+    <div>
+        <h2>Technical Profile</h2>
+    </div>
+  )
+}
+
+export default ModalProfileTechnical
